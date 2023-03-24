bc59e3f4584416d17a8f09e25721e7b36309cf73
koslinj <koslinj@gmail.com>
2023-03-24 18:13:32 +0100

    version 1.2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3003/api'
+    baseURL: process.env.API_URL || 'http://localhost:3003/api'
 });
 
 export default instance;
