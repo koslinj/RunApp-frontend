b85d7b40d6f5408f16f5689780bcc0fb39a65b19
koslinj <koslinj@gmail.com>
2023-03-25 14:28:18 +0100

    version 1.5.2

--- src/Chart/Chart.jsx
+++ src/Chart/Chart.jsx
@@ -3,46 +3,44 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 function Chart(props) {
 
-  const [bartekData, setBartekData] = useState([]);
-  const [janekData, setJanekData] = useState([]);
-
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const all = props.data;
-    const bartek = all.filter((el) => el.name === "Bartek").sort((a, b) => a.kilometers - b.kilometers);
-    const janek = all.filter((el) => el.name === "Janek").sort((a, b) => a.kilometers - b.kilometers);
-    handleFilter(bartek, setBartekData)
-    handleFilter(janek, setJanekData)
+    console.log(all);
+    processData(all);
+
   }, [props.data])
 
+  function processData(all) {
+    // Initialize a Set to keep track of unique kilometers
+    const kilometersSet = new Set(all.map(entry => entry.kilometers));
 
-  function handleFilter(data, func) {
-    const seenKilometers = {};
-    const filteredData = [];
+    // Initialize a Map to store the output data
+    const outputMap = new Map(Array.from(kilometersSet).map(kilometers => [kilometers, { kilometers, Janek: null, Bartek: null }]));
 
-    for (const obj of data) {
-      if (!(obj.kilometers in seenKilometers)) {
-        filteredData.push(obj);
-        seenKilometers[obj.kilometers] = obj.paceInSeconds;
-      } else {
-        if (obj.paceInSeconds < seenKilometers[obj.kilometers]) {
-          filteredData.splice(
-            filteredData.findIndex(x => x.kilometers === obj.kilometers),
-            1,
-            obj
-          );
-          seenKilometers[obj.kilometers] = obj.paceInSeconds;
-        }
+    // Loop through the input array and populate the output Map
+    all.forEach(entry => {
+      const outputEntry = outputMap.get(entry.kilometers);
+      if (entry.name === "Janek") {
+        outputEntry.Janek = entry.paceInSeconds;
+      } else if (entry.name === "Bartek") {
+        outputEntry.Bartek = entry.paceInSeconds;
       }
-    }
-    console.log(filteredData)
-    func(filteredData);
+    });
+
+    // Output the result as an array
+    const outputArray = Array.from(outputMap.values());
+    outputArray.sort((a, b) => a.kilometers - b.kilometers);
+    setData(outputArray);
+    console.log(outputArray);
   }
 
   return (
     <div className="bg-white rounded-lg max-w-2xl mx-auto mt-6 py-6 shadow-1">
       <ResponsiveContainer width="100%" height={350}>
         <LineChart
+          data={data}
           margin={{
             top: 5,
             right: 30,
@@ -53,10 +51,10 @@ function Chart(props) {
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis label={{ value: 'Rodzaj Biegu', position: 'bottom', offset: '-6' }} dataKey="kilometers" domain={['dataMin', 'dataMax']} type="number" tickFormatter={(x) => x + "km"} />
           <YAxis label={{ value: 'Tempo', angle: -90, position: 'left', offset: '-10' }} type='number' domain={['dataMin', 'dataMax']} tickCount={8} tickFormatter={(x) => Math.floor(x / 60) + ":" + String(x % 60).padStart(2, '0')} />
-          <Tooltip name="lol" formatter={(value) => Math.floor(value / 60) + ":" + String(value % 60).padStart(2, '0')} labelFormatter={(x) => x + "km"} />
+          <Tooltip formatter={(value) => Math.floor(value / 60) + ":" + String(value % 60).padStart(2, '0')} labelFormatter={(x) => x + "km"} />
           <Legend verticalAlign='top' />
-          <Line name="Jasiel" type="monotone" strokeWidth={3} dot={{ strokeWidth: 2 }} data={janekData} dataKey="paceInSeconds" stroke="#8884d8" />
-          <Line name="Bartoszek" type="monotone" strokeWidth={3} dot={{ strokeWidth: 2 }} data={bartekData} dataKey="paceInSeconds" stroke="#82ca9d" />
+          <Line connectNulls="true" name="Jasiel" type="monotone" strokeWidth={3} dot={{ strokeWidth: 2 }} dataKey="Janek" stroke="#8884d8" />
+          <Line connectNulls="true" name="Bartoszek" type="monotone" strokeWidth={3} dot={{ strokeWidth: 2 }} dataKey="Bartek" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
     </div>
